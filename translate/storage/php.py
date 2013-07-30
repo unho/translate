076c1c0adcb1094f24e6c778b08a84d4726fd114
Leandro Regueiro <leandro.regueiro@gmail.com>
2013-07-30 09:54:40 +0200

    Cleanup: Rename colonpos var to enddelpos
    It is makes more sense that the position of enddel is called enddelpos and
    not colonpos, specially because the end delimiter is not always a colon.

--- translate/storage/php.py
+++ translate/storage/php.py
@@ -365,23 +365,23 @@ class phpfile(base.TranslationStore):
                 if invalue:
                     value = line
 
-            # Get the end delimiter position (colonpos)
-            colonpos = value.rfind(enddel)
+            # Get the end delimiter position.
+            enddelpos = value.rfind(enddel)
 
-            while colonpos != -1:
+            while enddelpos != -1:
                 # Check if the latest non-whitespace character before the end
                 # delimiter is the valuequote
-                if value[:colonpos].rstrip()[-1] == valuequote:
+                if value[:enddelpos].rstrip()[-1] == valuequote:
                     # Save the value string without trailing whitespaces and
                     # without the ending quotes
-                    newunit.value = lastvalue + value[:colonpos].rstrip()[:-1]
+                    newunit.value = lastvalue + value[:enddelpos].rstrip()[:-1]
                     newunit.escape_type = valuequote
                     lastvalue = ""
                     invalue = False
 
                 # If there is more text (a comment) after the translation.
-                if not invalue and colonpos != (len(value) - 1):
-                    commentinlinepos = value.find("//", colonpos)
+                if not invalue and enddelpos != (len(value) - 1):
+                    commentinlinepos = value.find("//", enddelpos)
                     if commentinlinepos != -1:
                         newunit.addnote(value[commentinlinepos+2:].strip(),
                                         "developer")
@@ -393,9 +393,9 @@ class phpfile(base.TranslationStore):
                     value = ""
                     newunit = phpunit()
 
-                # Update end delimiter position (colonpos) to the previous last
-                # appearance of end delimiter.
-                colonpos = value.rfind(enddel, 0, colonpos)
+                # Update end delimiter position to the previous last appearance
+                # of the end delimiter.
+                enddelpos = value.rfind(enddel, 0, enddelpos)
             else:
                 # After processing current line, if we are non in an array,
                 # fall back to default dialect (PHP simple variable syntax).
