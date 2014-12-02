9ccb11156122e8faf451cecde9a37090c72d42ff
Leandro Regueiro <leandro.regueiro@gmail.com>
2014-12-02 13:00:42 +0100

    UDNO

--- translate/storage/test_jsonl10n.py
+++ translate/storage/test_jsonl10n.py
@@ -50,3 +50,82 @@ class TestJSON(test_monolingual.TestMonolingualStore):
 """
         regen = self.format_regen(source)
         assert source == "\n" + regen + "\n"
+
+#    def test_unicode_escaping(self):
+#        """Check that escaped unicode is converted properly."""
+#        propsource = "unicode=\u0411\u0416\u0419\u0428"
+#        messagevalue = u'\u0411\u0416\u0419\u0428'.encode("UTF-8")
+#        propfile = self.format_parse(propsource, personality="mozilla")
+#        assert len(propfile.units) == 1
+#        propunit = propfile.units[0]
+#        assert propunit.name == "unicode"
+#        assert propunit.source.encode("UTF-8") == "БЖЙШ"
+#        regensource = str(propfile)
+#        assert messagevalue in regensource
+#        assert "\\u" not in regensource
+
+#    def test_newlines_startend(self):
+#        """Check that we preserve \n that appear at start and end of properties."""
+#        propsource = "newlines=\\ntext\\n"
+#        propregen = self.format_regen(propsource)
+#        assert propsource + '\n' == propregen
+
+#    def test_key_value_delimiters_simple(self):
+#        """Check that we can handle colon, equals and space delimiter
+#        between key and value.  We don't test any space removal or escaping."""
+#        delimiters = [":", "=", " "]
+#        for delimiter in delimiters:
+#            propsource = "key%svalue" % delimiter
+#            print("source: '%s'\ndelimiter: '%s'" % (propsource, delimiter))
+#            propfile = self.format_parse(propsource)
+#            assert len(propfile.units) == 1
+#            propunit = propfile.units[0]
+#            assert propunit.name == "key"
+#            assert propunit.source == "value"
+
+#    def test_comments(self):
+#        """Check that we handle # and ! comments."""
+#        markers = ['#', '!']
+#        for comment_marker in markers:
+#            propsource = '''%s A comment
+#    key=value
+#    ''' % comment_marker
+#            propfile = self.format_parse(propsource)
+#            print(repr(propsource))
+#            print("Comment marker: '%s'" % comment_marker)
+#            assert len(propfile.units) == 1
+#            propunit = propfile.units[0]
+#            assert propunit.comments == ['%s A comment' % comment_marker]
+
+#    def test_trailing_comments(self):
+#        """Check that we handle non-unit data at the end of a file."""
+#        source = """
+#{
+#    "firstName": "John",
+#    "lastName": "Smith",
+#    "age": 25,
+#    "address": {
+#        "streetAddress": "21 2nd Street",
+#        "city": "New York",
+#        "state": "NY",
+#        "postalCode": "10021"
+#    },
+#    "phoneNumber": [
+#        {
+#          "type": "home",
+#          "number": "212 555-1234"
+#        },
+#        {
+#          "type": "fax",
+#          "number": "646 555-4567"
+#        }
+#    ]
+#}
+#"""
+#        propsource = u"key = value\n# END"
+#        propfile = self.format_parse(propsource)
+#        assert len(propfile.units) == 2
+#        propunit = propfile.units[1]
+#        assert propunit.name == u''
+#        assert propunit.source == u''
+#        assert propunit.getnotes() == u"# END"
